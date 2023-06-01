ef84601933c59dd8ccffe16dc0c3f3773d199182
hqunted <berkekirci@gmail.com>
2023-06-01 16:06:16 +0200

    feat: fix too many markers to just one

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,16 +4,18 @@ import { addMarker } from "./addMarker";
 import { readUrlParams } from "./readUrlParams";
 import MainModal from "./MainModal";
 import classNames from "classnames";
+import { useOnMapClick } from "../hooks/useOnMapClick";
 
 export const Map = () => {
   const [pickedEvents, setPickedEvents] = useState<string[]>([]);
   const [modalVisible, setModalVisible] = useState(false);
-
+  const { modalStatus, setModalStatus } = useOnMapClick();
   const formDataRef = useRef({
     title: "",
     description: "",
     date: "",
     time: "",
+    publishClicked: false,
   });
 
   const latString = readUrlParams()?.lat;
@@ -36,6 +38,7 @@ export const Map = () => {
     description: string;
     date: string;
     time: string;
+    publishClicked: boolean;
   }) => {
     // Do something with the data
     console.log("Title:", data.title);
@@ -92,21 +95,21 @@ export const Map = () => {
 
     const onMapClick = (event: LeafletMouseEvent) => {
       const { lat, lng } = event.latlng;
-      console.log(formDataRef.current.title === "");
+      console.log(event);
       // Show the modal
       if (formDataRef.current.title === "") {
         setModalVisible(true);
       } else {
         setModalVisible(false);
       }
-      const { title, description, date, time } = formDataRef.current; // Access the form data from the ref
 
-      if (title && description && time && date) {
-        const eventString = `${title}|${description}|${time}|${date}`;
-        const updatedPickedEvents = [...pickedEvents, eventString];
+      const { title, description, date, time, publishClicked } =
+        formDataRef.current; // Access the form data from the ref
+      const eventString = `${title}|${description}|${time}|${date}`;
+      const updatedPickedEvents = [...pickedEvents, eventString];
 
-        setPickedEvents(updatedPickedEvents);
-
+      setPickedEvents(updatedPickedEvents);
+      if (publishClicked) {
         const queryParams = new URLSearchParams({
           events: updatedPickedEvents.join(","),
           lat: String(lat),
@@ -114,20 +117,19 @@ export const Map = () => {
         }).toString();
         const url = `${window.location.origin}${window.location.pathname}?${queryParams}`;
         L.marker([lat, lng], { icon: customIcon })
+          .addTo(map)
           .bindPopup(
             `<b>Title:</b> ${title}<br><b>Description:</b> ${description}<br><br><b>Date:</b> ${date}<br><b>Time:</b> ${time}<br><b>Google Maps link:</b> <a href="${generateGoogleMapsLink(
               lat,
               lng
             )}" target="_blank" rel="noopener noreferrer">Open in Google Maps</a><br> <b>Url:</b> ${url}<br>`
-          )
-          .addTo(map);
+          );
+        map.off("click", onMapClick);
       }
     };
 
     map.on("click", onMapClick);
-
     return () => {
-      map.off("click", onMapClick);
       map.remove();
     };
   }, []);
