f5bf7455ae7bc5a32b0b076b08815b3d97b8cfa3
hqunted <berkekirci@gmail.com>
2023-06-01 20:12:30 +0200

    feat: declare marker variable and return

--- src/components/addMarker.tsx
+++ src/components/addMarker.tsx
@@ -16,8 +16,8 @@ export const addMarker = (
   time?: any,
   date?: any
 ) => {
-  if (readUrlParams())
-    L.marker([lat, lng], { icon: customIcon })
+  if (readUrlParams()) {
+    const marker = L.marker([lat, lng], { icon: customIcon })
       .bindPopup(
         `<b>Title:</b> ${title}<br><b>Description:</b> ${description}<br><br><b>Date:</b> ${date}<br><b>Time:</b> ${time}<br><b>Google Maps link:</b> <a href="${generateGoogleMapsLink(
           lat,
@@ -25,4 +25,7 @@ export const addMarker = (
         )}" target="_blank" rel="noopener noreferrer">Open in Google Maps</a><br>`
       )
       .addTo(map);
+
+    return marker; // Return the marker object
+  }
 };
