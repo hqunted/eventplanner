fc2800710728cfb0cda3b5f27a4b1893cb7be299
hqunted <berkekirci@gmail.com>
2023-06-03 15:23:03 +0200

    feat: delete closeButton

--- src/components/mainModal.tsx
+++ src/components/mainModal.tsx
@@ -46,12 +46,6 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
     setModalVisibility("invisible");
   };
 
-  const handleCloseClick = () => {
-    if (!publishClicked) {
-      setModalVisibility("invisible");
-    }
-  };
-
   return (
     <div
       className={classNames(
@@ -59,18 +53,18 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
         modalVisibility
       )}
     >
-      <div className="max-w-md w-full bg-white rounded-xl overflow-hidden shadow-lg">
-        <div className="flex justify-end pr-2">
-          <button
-            type="button"
-            onClick={handleCloseClick}
-            className="p-2 text-gray-500 hover:text-green-700 focus:outline-none text-lg"
-          >
-            x
-          </button>
+      <div className="max-w-md w-full bg-slate-100 rounded-xl overflow-hidden shadow-lg">
+        <div className="flex justify-end ">
+          <div className="flex justify-center items-center flex-grow">
+            <img
+              src={require("../assets/formlogo.png")}
+              alt="Form Logo"
+              className="h-36 w-28 pt-8"
+            />
+          </div>
         </div>
 
-        <form className="p-8 pt-4 space-y-6" onSubmit={handleSubmit}>
+        <form className="p-8 pt-2 space-y-4" onSubmit={handleSubmit}>
           <input type="hidden" name="remember" defaultValue="true" />
           <div className="space-y-4">
             <div>
@@ -88,7 +82,7 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
                 required
                 onChange={handleChange}
                 value={title}
-                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 placeholder="Please enter the Title of your activity"
               />
             </div>
@@ -107,7 +101,7 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
                 rows={4}
                 onChange={handleChange}
                 value={description}
-                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 placeholder="Please enter the description of your activity"
               ></textarea>
             </div>
@@ -127,7 +121,7 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
                   required
                   onChange={handleChange}
                   value={date}
-                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 />
               </div>
               <div>
@@ -145,12 +139,12 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
                   required
                   onChange={handleChange}
                   value={time}
-                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 />
               </div>
             </div>
           </div>
-          <label htmlFor="time" className="block text-green-500 font-medium">
+          <label htmlFor="time" className="block text-blue-500 font-medium">
             (After clicking the button. Please click on the map to choose the
             location of your event.)
           </label>
@@ -161,7 +155,7 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
               onClick={() => {
                 handlePublishClick();
               }}
-              className=" w-full py-2 px-4 mt-6 bg-green-500 hover:bg-green-600 text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+              className=" w-full py-2 px-4 mt-6 bg-blue-500 hover:bg-blue-600 text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
             >
               Choose the location
             </button>
