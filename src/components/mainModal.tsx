c0b8809c7111a6d1481f84f29175661d11b01eaa
hqunted <berkekirci@gmail.com>
2023-06-10 12:31:53 +0200

    feat: implement responsive design part1

--- src/components/mainModal.tsx
+++ src/components/mainModal.tsx
@@ -49,30 +49,33 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
   return (
     <div
       className={classNames(
-        "min-h-screen bg-gray-700 flex items-center opacity-95 justify-center px-4 sm:px-6 lg:px-8",
+        "min-h-screen bg-gray-700 flex items-center opacity-95 justify-center",
         modalVisibility
       )}
     >
-      <div className="max-w-md w-full bg-slate-100 rounded-xl overflow-hidden shadow-lg">
-        <div className="flex justify-end ">
+      <div className=" bg-slate-100 rounded-xl overflow-hidden shadow-lg w-[95%] md:w-[75%] lg:w-[30%]">
+        <div className="flex justify-end">
           <div className="flex justify-center items-center flex-grow">
             <img
               src={require("../assets/formlogo.png")}
               alt="Form Logo"
-              className="h-36 w-28 pt-8"
+              className="h-96 w-68 pt-16 md:h-72 md:w-56"
             />
           </div>
         </div>
 
-        <form className="p-8 pt-2 space-y-4" onSubmit={handleSubmit}>
+        <form
+          className="p-16 pt-8 space-y-8 md:p-12 md:pt-2"
+          onSubmit={handleSubmit}
+        >
           <input type="hidden" name="remember" defaultValue="true" />
-          <div className="space-y-4">
+          <div className="space-y-4 md:space-y-4">
             <div>
               <label
                 htmlFor="title"
-                className="block text-gray-700 font-medium"
+                className="block text-gray-700 font-medium text-5xl sm:mb-4 sm:mt-4 sm:text-5xl md:text-4xl xl:text-sm "
               >
-                Title
+                Title:
               </label>
               <input
                 id="title"
@@ -82,16 +85,16 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
                 required
                 onChange={handleChange}
                 value={title}
-                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-md relative block w-full sm:px-8 sm:py-8 lg:py-2 lg:px-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 text-4xl sm:text-5xl md:text-2xl lg:text-sm"
                 placeholder="Please enter the Title of your activity"
               />
             </div>
             <div>
               <label
                 htmlFor="description"
-                className="block text-gray-700 font-medium"
+                className="block text-gray-700 font-medium text-5xl sm:mb-4 sm:mt-12 sm:text-5xl md:mb-2 md:mt-6 md:text-4xl lg:text-sm"
               >
-                Description
+                Description:
               </label>
               <textarea
                 id="description"
@@ -101,17 +104,17 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
                 rows={4}
                 onChange={handleChange}
                 value={description}
-                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-md relative block w-full sm:px-8 sm:py-8 md:mb-2 md:mt-4 lg:py-2 lg:px-2  border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 text-4xl sm:text-5xl md:text-2xl lg:text-sm "
                 placeholder="Please enter the description of your activity"
               ></textarea>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <div className="grid grid-cols-2 sm:grid-cols-2 gap-4 ">
               <div>
                 <label
                   htmlFor="date"
-                  className="block text-gray-700 font-medium"
+                  className="block text-gray-700 font-medium text-5xl sm:mb-4 sm:mt-10 sm:text-5xl md:mb-2 md:mt-4 md:text-4xl lg:text-sm"
                 >
-                  Date
+                  Date:
                 </label>
                 <input
                   id="date"
@@ -121,15 +124,15 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
                   required
                   onChange={handleChange}
                   value={date}
-                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-md relative block w-full sm:px-8 sm:py-8 md:mb-2 md:mt-4 lg:py-2 lg:px-2  border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 text-4xl sm:text-5xl md:text-2xl lg:text-sm"
                 />
               </div>
               <div>
                 <label
                   htmlFor="time"
-                  className="block text-gray-700 font-medium"
+                  className="block text-gray-700 font-medium text-5xl sm:mb-4 sm:mt-10 sm:text-5xl md:mb-2 md:mt-4 md:text-4xl lg:text-sm"
                 >
-                  Time
+                  Time:
                 </label>
                 <input
                   id="time"
@@ -139,12 +142,12 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
                   required
                   onChange={handleChange}
                   value={time}
-                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-md relative block w-full sm:px-8 sm:py-8 md:mb-2 md:mt-4 lg:py-2 lg:px-2  border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 text-4xl sm:text-5xl md:text-2xl lg:text-sm "
                 />
               </div>
             </div>
           </div>
-          <label htmlFor="time" className="block text-blue-500 font-medium">
+          <label className="block text-blue-500 font-medium pt-16 md:pt-8 text-4xl md:text-3xl lg:text-sm">
             (After clicking the button. Please click on the map to choose the
             location of your event.)
           </label>
@@ -155,7 +158,7 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
               onClick={() => {
                 handlePublishClick();
               }}
-              className=" w-full py-2 px-4 mt-6 bg-blue-500 hover:bg-blue-600 text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+              className="w-full px-6 py-8 mb-4 md:px-2 md:py-6 lg:py-2 lg:px-2 bg-blue-500 hover:bg-blue-600 text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 text-4xl sm:text-5xl md:text-3xl lg:text-sm"
             >
               Choose the location
             </button>
@@ -165,5 +168,4 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
     </div>
   );
 };
-
 export default MainModal;
