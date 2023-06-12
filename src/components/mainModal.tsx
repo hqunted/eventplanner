0d3d4a4c91932d3bb5fcc9f46779ae0531eff4ee
hqunted <berkekirci@gmail.com>
2023-06-12 14:21:20 +0200

    feat: add responsive design

--- src/components/mainModal.tsx
+++ src/components/mainModal.tsx
@@ -53,27 +53,27 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
         modalVisibility
       )}
     >
-      <div className=" bg-slate-100 rounded-xl overflow-hidden shadow-lg w-[95%] md:w-[85%] lg:w-[30%]">
+      <div className=" bg-slate-100 rounded-xl overflow-hidden shadow-lg w-[95%] md:w-[85%] lg:w-[40%]">
         <div className="flex justify-end">
           <div className="flex justify-center items-center flex-grow">
             <img
               src={require("../assets/formlogo.png")}
               alt="Form Logo"
-              className="h-96 w-68 pt-16 md:h-72 md:w-56 lg:h-64 lg:w-48"
+              className="h-96 w-68 pt-16 md:h-72 md:w-56 lg:h-48 lg:w-36 lg:pt-2"
             />
           </div>
         </div>
 
         <form
-          className="p-16 pt-8 space-y-8 md:p-12 md:pt-2"
+          className="p-16 pt-8 space-y-8 md:p-12 md:pt-2 lg:space-y-2  "
           onSubmit={handleSubmit}
         >
           <input type="hidden" name="remember" defaultValue="true" />
-          <div className="space-y-4 md:space-y-4">
+          <div className="space-y-4 md:space-y-4 lg:space-y-2">
             <div>
               <label
                 htmlFor="title"
-                className="block text-gray-700 font-medium text-5xl sm:mb-4 sm:mt-4 sm:text-5xl md:text-4xl xl:text-sm "
+                className="block text-gray-700 font-medium text-5xl sm:mb-4 sm:mt-4 sm:text-5xl md:text-3xl lg:text-sm xl:text-sm "
               >
                 Down goes the Title👇:
               </label>
@@ -85,14 +85,14 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
                 required
                 onChange={handleChange}
                 value={title}
-                className="appearance-none rounded-md relative block w-full sm:px-8 sm:py-8 lg:py-2 lg:px-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 text-4xl sm:text-5xl md:text-2xl lg:text-sm"
+                className="appearance-none rounded-md relative block w-full sm:px-8 sm:py-8 lg:py-2 lg:px-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 text-4xl sm:text-5xl md:text-3xl lg:text-sm"
                 placeholder="Please enter the title of your adventure"
               />
             </div>
             <div>
               <label
                 htmlFor="description"
-                className="block text-gray-700 font-medium text-5xl sm:mb-4 sm:mt-12 sm:text-5xl md:mb-2 md:mt-6 md:text-4xl lg:text-sm"
+                className="block text-gray-700 font-medium text-5xl sm:mb-4 sm:mt-12 sm:text-5xl md:mb-2 md:mt-6 md:text-3xl lg:text-sm"
               >
                 What are you planning im curious👀👀:
               </label>
@@ -104,7 +104,7 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
                 rows={4}
                 onChange={handleChange}
                 value={description}
-                className="appearance-none rounded-md relative block w-full sm:px-8 sm:py-8 md:mb-2 md:mt-4 lg:py-2 lg:px-2  border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 text-4xl sm:text-5xl md:text-2xl lg:text-sm "
+                className="appearance-none rounded-md relative block w-full sm:px-8 sm:py-8 md:mb-2 md:mt-4 lg:py-2 lg:px-2  border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 text-4xl sm:text-5xl md:text-3xl lg:text-sm "
                 placeholder="Please enter the description of your adventure"
               ></textarea>
             </div>
@@ -112,7 +112,7 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
               <div>
                 <label
                   htmlFor="date"
-                  className="block text-gray-700 font-medium text-5xl sm:mb-4 sm:mt-10 sm:text-5xl md:mb-2 md:mt-4 md:text-4xl lg:text-sm"
+                  className="block text-gray-700 font-medium text-5xl sm:mb-4 sm:mt-10 sm:text-5xl md:mb-2 md:mt-4 md:text-3xl lg:text-sm"
                 >
                   Picking a date would be nice📅📅:
                 </label>
@@ -124,13 +124,13 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
                   required
                   onChange={handleChange}
                   value={date}
-                  className="appearance-none rounded-md relative block w-full sm:px-8 sm:py-8 md:mb-2 md:mt-4 lg:py-2 lg:px-2  border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 text-4xl sm:text-5xl md:text-2xl lg:text-sm"
+                  className="appearance-none rounded-md relative block w-full sm:px-8 sm:py-8 md:mb-2 md:mt-4 lg:py-2 lg:px-2  border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 text-4xl sm:text-5xl md:text-3xl lg:text-sm"
                 />
               </div>
               <div>
                 <label
                   htmlFor="time"
-                  className="block text-gray-700 font-medium text-5xl sm:mb-4 sm:mt-10 sm:text-5xl md:mb-2 md:mt-4 md:text-4xl lg:text-sm"
+                  className="block text-gray-700 font-medium text-5xl sm:mb-4 sm:mt-10 sm:text-5xl md:mb-2 md:mt-4 md:text-3xl lg:text-sm"
                 >
                   Tick tack time passes⏱⏱:
                 </label>
@@ -142,7 +142,7 @@ const MainModal = ({ handleFormSubmit }: MainModalProps) => {
                   required
                   onChange={handleChange}
                   value={time}
-                  className="appearance-none rounded-md relative block w-full sm:px-8 sm:py-8 md:mb-2 md:mt-4 lg:py-2 lg:px-2  border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 text-4xl sm:text-5xl md:text-2xl lg:text-sm "
+                  className="appearance-none rounded-md relative block w-full sm:px-8 sm:py-8 md:mb-2 md:mt-4 lg:py-2 lg:px-2  border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 text-4xl sm:text-5xl md:text-3xl lg:text-sm "
                 />
               </div>
             </div>
